63bbd8fc6ef1717e9eb2e38b12e8bd6c6e0a85e6
negin <negin.s.mousavi@gmail.com>
2021-12-26 19:14:34 +0330

    add hibernate

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,11 +1,21 @@
 package model;
 
 import enumuration.Gender;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * @author Negin Mousavi
  */
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String firstName;
     private String lastName;
